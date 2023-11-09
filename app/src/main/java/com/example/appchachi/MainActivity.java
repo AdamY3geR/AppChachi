98858420483c476966f7a7175f7fd33bdec2ace4
adamKing <adamyeger@gmail.com>
2023-11-09 13:25:15 +0200

    I added 3 buttons

--- app/src/main/java/com/example/appchachi/MainActivity.java
+++ app/src/main/java/com/example/appchachi/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.appchachi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.navi_dashboard:
+                        selectedFragment = new DashboardFragment();
+                        break;
+                    case R.id.navi_messages:
+                        selectedFragment = new MesseagesFragment();
+                        break;
+                    case R.id.navi_calendar:
+                        selectedFragment = new CalendarFragment();
+                        break;
+                    case R.id.navi_chachi_group:
+                        selectedFragment = new ChachiGroupFragment();
+                        break;
+                }
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.container, selectedFragment);
+                transaction.commit();
+                return true;
+            }
+        });
+        // Set the initial fragment
+        bottomNavigationView.setSelectedItemId(R.id.navi_dashboard);
     }
 }
