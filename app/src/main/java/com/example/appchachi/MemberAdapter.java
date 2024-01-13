16702c34aa834483685cef113516dcfd18493d1e
adamY3geR <adamyeger@gmail.com>
2024-01-13 20:39:48 +0200

    blalba

--- app/src/main/java/com/example/appchachi/MemberAdapter.java
+++ app/src/main/java/com/example/appchachi/MemberAdapter.java
@@ -1,17 +1,20 @@
 package com.example.appchachi;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
 public class MemberAdapter extends ArrayAdapter {
-    public MemberAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull List objects) {
-        super(context, resource, textViewResourceId, objects);
-    }
     Context context;
     List<Member> objects;
+    public MemberAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull List<Member> objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
 
 }
